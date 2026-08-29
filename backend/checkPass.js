362eb2ba34dfc25e1e2726c9cc0050913e664ee5
Pradeep Yadav <pradeep.yadav.cs27@iilm.edu>
2026-08-30 02:13:29 +0530

    Delete backend directory

--- backend/checkPass.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-async function checkPass() {
-  const isMatch = await bcrypt.compare('Teacher@123', '$2a$10$dhGh.t2dAWGSBEnpDbsA.O20uZr5WeQlFk0IGL0lG7xEgN6uH9rJu');
-  console.log('Match Teacher@123?', isMatch);
-}
-checkPass();
